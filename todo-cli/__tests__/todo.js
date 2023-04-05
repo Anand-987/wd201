f469fc77ca69e880d9bdc1a42751844363d1209b
Anand <gugulothanand1@gamil.com>
2023-04-05 20:31:53 +0530

    changes in todo.js

--- todo-cli/__tests__/todo.js
+++ todo-cli/__tests__/todo.js
@@ -1,13 +1,28 @@
 const todoList = require("../todo");
 
-const { all, markAsComplete, add } = todoList();
+const { all, overdue, dueToday, dueLater, markAsComplete, add } = todoList();
 
 describe("Todolist Test Suite", () => {
   beforeAll(() => {
+    const today = new Date();
     add({
-      title: "Test todo",
+      title: "hospital checkup",
       completed: false,
       dueDate: new Date().toLocaleDateString("en-CA"),
+    });
+    add({
+      title: "assignment",
+      completed: false,
+      dueDate: new Date(
+        today.getTime() - 60 * 60 * 24 * 1000
+      ).toLocaleDateString("en-CA"),
+    });
+    add({
+      title: "meeting",
+      completed: false,
+      dueDate: new Date(
+        today.getTime() + 60 * 60 * 24 * 1000
+      ).toLocaleDateString("en-CA"),
     });
   });
   test("Should add new todo", () => {
@@ -23,5 +38,14 @@ describe("Todolist Test Suite", () => {
     expect(all[0].completed).toBe(false);
     markAsComplete(0);
     expect(all[0].completed).toBe(true);
+  });
+  test("overdue retrival", () => {
+    expect(overdue().length).toBe(1);
+  });
+  test("dueToday", () => {
+    expect(dueToday().length).toBe(2);
+  });
+  test("dueLater", () => {
+    expect(dueLater().length).toBe(1);
   });
 });
--- todo-cli/todo.js
+++ todo-cli/todo.js
@@ -1,10 +1,4 @@
-/* eslint-disable eol-last */
-/* eslint-disable quotes */
-/* eslint-disable eqeqeq */
-/* eslint-disable semi */
 /* eslint-disable no-undef */
-/* eslint-disable spaced-comment */
-/* eslint-disable indent */
 const todoList = () => {
   all = [];
   const add = (todoItem) => {
@@ -15,54 +9,32 @@ const todoList = () => {
   };
 
   const overdue = () => {
-    // Write the date check condition here and return the array
-    // of overdue items accordingly.
-    //l=[]
-    //for (let i = 0; i<all.length;i++){
-    //    l.push(all.filter(all => all[i].dueDate = yesterday))
-    //}
-    return all.filter((all) => all.dueDate === yesterday);
+    return all.filter(
+      (item) => item.dueDate < new Date().toLocaleDateString("en-CA")
+    );
   };
 
   const dueToday = () => {
-    // Write the date check condition here and return the array
-    // of todo items that are due today accordingly.
-    return all.filter((all) => all.dueDate === today);
+    return all.filter(
+      (item) => item.dueDate === new Date().toLocaleDateString("en-CA")
+    );
   };
 
   const dueLater = () => {
-    // Write the date check condition here and return the array
-    // of todo items that are due later accordingly.
-    return all.filter((all) => all.dueDate === tomorrow);
+    return all.filter(
+      (item) => item.dueDate > new Date().toLocaleDateString("en-CA")
+    );
   };
 
   const toDisplayableList = (list) => {
-    // Format the To-Do list here, and return the output string
-    // as per the format given above.
-    let str = "";
-    j = 0;
-    for (let i = 0; i < list.length; i++) {
-      if (list[i].completed == false && list[i].dueDate != today) {
-        if (i == list.length - 1) {
-          str += "[ ] " + list[i].title + " " + list[i].dueDate;
-        } else {
-          str += "[ ] " + list[i].title + " " + list[i].dueDate + "\n";
-        }
-      } else if (list[i].completed == true && list[i].dueDate == today) {
-        if (i == list.length - 1) {
-          str += "[x] " + list[i].title + " ";
-        } else {
-          str += "[x] " + list[i].title + "\n";
-        }
-      } else if (list[i].completed == false && list[i].dueDate == today) {
-        if (i == list.length - 1) {
-          str += "[ ] " + list[i].title + " ";
-        } else {
-          str += "[ ] " + list[i].title + "\n";
-        }
-      }
-    }
-    return str;
+    const completionStatus = item.completed ? "[x]" : "";
+    const displayedDate =
+      item.dueDate === new Date().toISOString().slice(0, 10)
+        ? ""
+        : item.dueDate;
+    return list.map(
+      (item) => $(completionStatus) + $(item.title) + $(displayedDate)
+    );
   };
 
   return {
@@ -75,5 +47,4 @@ const todoList = () => {
     toDisplayableList,
   };
 };
-
 module.exports = todoList;
