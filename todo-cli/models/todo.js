c5d763dda4d0c9327240a5e4cea7d7a91d9ea447
Anand-987 <119809987+Anand-987@users.noreply.github.com>
2023-04-09 18:00:01 +0530

    Update todo.js

--- todo-cli/models/todo.js
+++ todo-cli/models/todo.js
@@ -74,7 +74,7 @@ module.exports = (sequelize, DataTypes) => {
     displayableString () {
       let checkbox = this.completed ? "[x]" : "[ ]";
       let date = 
-          this.dueDate === new Date().toLocaleDateString("en-CA") ? "" : this.dueDate;
+          this.dueDate === new Date().toISOString().slice(0, 10) ? "" : this.dueDate;
       return `${this.id}. ${checkbox} ${this.title} ${date}`.trim();
     }
   }
